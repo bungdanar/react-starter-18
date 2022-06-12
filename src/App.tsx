6d7d2f3c2245b930e78aa68bef4a0ab84f8c256a
Bung Danar <danar.g.putera@gmail.com>
2022-06-12 19:00:18 +0700

    chore: Add bootstrap package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CustomCard from './components/custom-card/CustomCard'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <div className='row justify-content-center'>
+        <div className='col-sm-6'>
+          <CustomCard>Initial App</CustomCard>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
